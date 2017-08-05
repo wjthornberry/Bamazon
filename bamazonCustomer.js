3ec318810f6672c3c592750a1557066df855ff21
Jake Thornberry <wjthornberry@gmail.com>
2017-08-05 08:06:47 -0500

    add readProducts f(x)

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -112,3 +112,13 @@ function deleteProduct() {
         }
     );
 }
+
+function readProducts() {
+    console.log('Selecting all products...\n');
+    connection.query('SELECT * FROM bamazon_DB', function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        connection.end();
+    });
+}

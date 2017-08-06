9d17dd008dfb1ff83137873fc3049ba7e0026f2c
Jake Thornberry <wjthornberry@gmail.com>
2017-08-05 20:00:20 -0500

    DB working in Sequel Pro

--- bamazon_DB.sql
+++ bamazon_DB.sql
@@ -1,11 +1,11 @@
--- Drops the bamazon_SQL_db if it exists currently --
-DROP DATABASE IF EXISTS bamazon_SQL_db;
+-- Drops the Bamazon database if it exists currently --
+DROP DATABASE IF EXISTS Bamazon;
 
--- Creates the "bamazon_SQL_db" database --
-CREATE DATABASE bamazon_SQL_db;
+-- Creates the Bamazon database --
+CREATE DATABASE Bamazon;
 
--- All of the following code will affect bamazon_SQL_db -- 
-USE bamazon_SQL_db;
+-- All of the following code will affect Bamazon -- 
+USE Bamazon;
 
 -- Creates the "products" table within bamazon_SQL_db --
 CREATE TABLE products (
@@ -17,7 +17,7 @@ CREATE TABLE products (
     -- department's name -- 
     department_name VARCHAR(255) NOT NULL,
     -- item's cost to customer; max number of digits is 10 and two of the digits will be decimals --
-    price DECIMAL(10,2), NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
     -- stock quantity, or how much of the product is available in stores --
     stock_quantity INT(255) NOT NULL,
     -- Sets the primary key of the table to item_id --
@@ -25,28 +25,27 @@ CREATE TABLE products (
 );
 
 -- Creates new rows containing data in all named columns --
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
 VALUES 
-("JL421 Badonkadonk Land Cruiser/Tank", "Everything Else", 19,999.95, 2),
+(1, "JL421 Badonkadonk Land Cruiser/Tank", "Everything Else", 19999.95, 2),
 
-("Custom Nicolas Cage Pillowcase Standard Size Design Cotton Pillow Case P-170", "Home and Kitchen", 6.00, 50),
+(2, "Custom Nicolas Cage Pillowcase Standard Size Design Cotton Pillow Case P-170", "Home and Kitchen", 6.00, 50),
 
-("Jeff Goldblum Custom Waterproof Shower Curtain 60x72 Inch Bath Curtains by LiangZP", "Home and Kitchen", 15.00, 10),
+(3, "Jeff Goldblum Custom Waterproof Shower Curtain 60x72 Inch Bath Curtains by LiangZP", "Home and Kitchen", 15.00, 10),
 
-("Duncan Butterfly Yo-Yo", "Toys and Games", 5.43, 20),
+(4, "Duncan Butterfly Yo-Yo", "Toys and Games", 5.43, 20),
 
-("Exploding Kittens: NSFW Edition", "Toys and Games", 19.99, 30),
+(5, "Exploding Kittens: NSFW Edition", "Toys and Games", 19.99, 30),
 
-("Zen Reflections Juniper Bonsai", "Patio, Lawn, and Garden", 26.99, 15),
+(6, "Zen Reflections Juniper Bonsai", "Patio, Lawn, and Garden", 26.99, 15),
 
-("Chemex 6-Cup Classic Series Glass Coffee Maker", "Kitchen & Dining", 37.82, 30),
+(7, "Chemex 6-Cup Classic Series Glass Coffee Maker", "Kitchen & Dining", 37.82, 30),
 
-("Blue Bottle Coffee - New Orleans Iced Kit", "Grocery & Gourmet Food", 36.99, 50),
+(8, "Blue Bottle Coffee - New Orleans Iced Kit", "Grocery & Gourmet Food", 36.99, 50),
 
-("You Don't Know JS: Up & Going", "Books", 4.99, 1),
+(9, "You Don't Know JS: Up & Going", "Books", 4.99, 1),
 
-("Kodak Portra 400 Professional ISO 400, 35mm, 36 Exposures, Color Negative Film 5 Roll per Pack", "Film", 43.90, 3),
+(10, "Kodak Portra 400 Professional ISO 400, 35mm, 36 Exposures, Color Negative Film 5 Roll per Pack", "Film", 43.90, 3);
 
-SELECT * FROM products;
 
 
